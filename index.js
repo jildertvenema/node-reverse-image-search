799a14c5e11ed5d913c9cf29bf3f19b5c1e8433c
Jildert Venema <j.venema@ziuz.com>
2020-08-28 13:50:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+module.exports = (async (imageUrl, callback) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(imageUrl));
+
+  const images = await page.evaluate(() => {
+    return Array.from(document.body.querySelectorAll('div div a h3')).slice(2).map(e => e.parentNode).map(el => ({ url: el.href, title: el.querySelector('h3').innerHTML }))
+  })
+  callback(images);
+  await browser.close();
+});
---
+++ test.js
@@ -0,0 +1,7 @@
+const reverseImageSearch =  require('./index');
+
+const doSomething = (res) => {
+  console.log(res)
+}
+
+reverseImageSearch('i.ebayimg.com/00/s/OTAwWDkwMA==/z/3G8AAOSwzoxd80XB/$_83.JPG', doSomething)
